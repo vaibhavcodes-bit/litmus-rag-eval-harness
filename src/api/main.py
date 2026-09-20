d9b938db56a30945a533ffcac2b4bc6afc148066
Vaibhav Pandey <pandeyvaibhav459@gmail.com>
2026-09-20 23:24:33 +0530

    Allow Vercel frontend CORS origin

--- src/api/main.py
+++ src/api/main.py
@@ -25,9 +25,10 @@ app = FastAPI(
 # http://localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-    ],
+   allow_origins=[
+    "http://localhost:3000",
+    "https://litmus-frontend-alpha.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
